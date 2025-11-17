c8b7cb13a2f2e7b2d14b726a38320102c0df7428
Gowtham Yarlagadda <gowtham777y@gmail.com>
2025-11-18 01:30:52 +0530

    Accessing OMDB API and getting the movie results with Title searched

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const baseURL = "http://www.omdbapi.com/?i=tt3896198&apikey=5768c681"
+
+const searchMovie = document.getElementById("search-movie")
+
+searchMovie.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const formData = new FormData(searchMovie)
+    const movieTitle = formData.get("movie-title")
+    getMoviesList(movieTitle)
+})
+
+async function getMoviesList(title){
+    const response = await fetch(baseURL+`&s=${title}`)
+    if(!response.ok){
+        console.log(`HTTP Error! status ${response.status}`)
+    }
+    const data = await response.json()
+    console.log(data)
+}
