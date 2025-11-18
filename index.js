a49ae70053437684e31a56560fd0ea4ec27b2317
Gowtham Yarlagadda <gowtham777y@gmail.com>
2025-11-19 01:03:02 +0530

    Add movies to watchList and render it in watchlist page

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const baseURL = "http://www.omdbapi.com/?i=tt3896198&apikey=5768c681"
 
 const searchMovie = document.getElementById("search-movie")
+const movieList = document.getElementById("movie-list")
+
+let movieArray = []
+let watchListMovies = []
 
 searchMovie.addEventListener("submit",(e)=>{
     e.preventDefault()
     const formData = new FormData(searchMovie)
     const movieTitle = formData.get("movie-title")
-    getMoviesList(movieTitle)
+    console.log(movieTitle)
+    if (movieTitle === "" || movieTitle === null){
+        movieList.innerHTML = `<p id="placeholder">🎬 Start Exploring</p>`
+    } else {
+        getMoviesList(movieTitle)
+    }
 })
 
 async function getMoviesList(title){
@@ -15,5 +24,58 @@ async function getMoviesList(title){
         console.log(`HTTP Error! status ${response.status}`)
     }
     const data = await response.json()
-    console.log(data)
+    movieArray = data.Search;
+    renderMovies(movieArray)
 }
+
+function renderMovies(movieArray){
+    const innerHTML = movieArray.map(movie => {
+        return `
+            <div class="movie">
+                <img class="image" src="${movie.Poster}">
+                <div>
+                    <h2 class="movie-name">${movie.Title}</h2>
+                    <div class="movie-info">
+                        <p>${movie.Year}</p>
+                        <p>Action, Drama, Scifi</p>
+                        <button data-imdbID="${movie.imdbID}">+ Wishlist</button>
+                    </div>
+                    <p class="movie-description">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</p>
+                </div>
+            </div>
+        `
+    }).join('')
+    movieList.innerHTML = innerHTML
+}
+
+document.addEventListener("click",function(e){
+    if (e.target.dataset.imdbID){
+        watchListMovies.push(movieArray.filter(function(movie){
+            return movie.imdbID === e.target.dataset.imdbID
+        }))
+        localStorage.setItem("movies",watchListMovies)
+    }
+})
+
+function renderWatchList(){
+    console.log(watchListMovies)
+    const innerHTML = watchListMovies.map(movie => {
+        return `
+            <div class="movie">
+                <img class="image" src="${movie.Poster}">
+                <div>
+                    <h2 class="movie-name">${movie.Title}</h2>
+                    <div class="movie-info">
+                        <p>${movie.Year}</p>
+                        <p>Action, Drama, Scifi</p>
+                        <button data-imdbID="${movie.imdbID}">+ Wishlist</button>
+                    </div>
+                    <p class="movie-description">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</p>
+                </div>
+            </div>
+        `
+    }).join('')
+    document.getElementById("watchlist").innerHTML = innerHTML
+}
+
+renderWatchList()
